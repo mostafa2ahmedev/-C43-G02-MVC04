21d05ef951ed950b42280d79ea13d008df118899
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-04-06 01:36:05 +0200

    Add project files.

---
+++ IKEA.DAL/Presistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using IKEA.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.DAL.Presistence.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
---
+++ IKEA.DAL/Presistence/Data/Configurations/Departments/DepartmentConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IKEA.DAL.Models;
+
+namespace IKEA.DAL.Presistence.Data.Configurations.Departments
+{
+    public class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            // Table name
+            builder.ToTable("Departments");
+
+            // Primary Key
+            builder.HasKey(d => d.Id);
+
+            // Properties
+            builder.Property(d => d.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(d => d.Code)
+                .IsRequired()
+                .HasMaxLength(10);
+
+            builder.Property(d => d.CreationDate)
+                .IsRequired();
+
+            // Computed column for "LastModifiedOn"
+            builder.Property(d => d.LastModifiedOn)
+                .HasComputedColumnSql("GETDATE()");
+
+            // Default value for "CreatedOn"
+            builder.Property(d => d.CreatedOn)
+                .HasDefaultValueSql("GETDATE()");
+        }
+    }
+}
